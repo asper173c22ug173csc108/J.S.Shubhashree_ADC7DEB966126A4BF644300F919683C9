2c5351c79c894877ca0b9e24ff4d503eb2a1dad8
asper173c22ug173csc108 <147733463+asper173c22ug173csc108@users.noreply.github.com>
2023-10-12 21:03:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask('app')
+@app.route('/')
+def hello_world():
+  return 'Hello, World!'
+app.run(host='0.0.0.0', port=8080)
+
+Factorial
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+print(factorial(5))
+
+Hello World￼
